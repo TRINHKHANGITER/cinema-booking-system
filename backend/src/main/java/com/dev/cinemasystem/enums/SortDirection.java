6c4f5425755075e3c4c6daa317a6171c86ccbc90
Vo Trinh Khang <trinhkhanggithub@gmail.com>
2026-04-13 00:32:56 +0700

    add format file

---
+++ backend/src/main/java/com/dev/cinemasystem/enums/SortDirection.java
@@ -0,0 +1,6 @@
+package com.dev.cinemasystem.enums;
+
+public enum SortDirection {
+    ASC,
+    DESC
+}
--- frontend/src/types/combo.d.ts
+++ frontend/src/types/combo.d.ts
@@ -1,11 +1,11 @@
 export interface comboFood {
-  comboId: number;
-  comboName: string;
-  description: string;
-  price: string;
-  status: number;
+    comboId: number;
+    comboName: string;
+    description: string;
+    price: string;
+    status: number;
 }
 
 export interface SelectedCombo extends comboFood {
-  quantity: number;
+    quantity: number;
 }
