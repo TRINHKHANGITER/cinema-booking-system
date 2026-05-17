dca7a67e3231ca9a8beb3f34f70749497224fc75
Vo Trinh Khang <trinhkhanggithub@gmail.com>
2026-05-17 16:23:46 +0700

    remove bad  code

--- backend/src/main/java/com/dev/cinemasystem/controller/ShowTimeSeatController.java
+++ backend/src/main/java/com/dev/cinemasystem/controller/ShowTimeSeatController.java
@@ -1,48 +1,4 @@
-package com.dev.cinemasystem.controller;
+﻿package com.dev.cinemasystem.controller;
 
-import com.dev.cinemasystem.dto.apiDTO.ApiResponse;
-import com.dev.cinemasystem.dto.showTimeSeatDTO.HoldSeatRequest;
-import com.dev.cinemasystem.dto.showTimeSeatDTO.HoldSeatResponse;
-import com.dev.cinemasystem.dto.showTimeSeatDTO.ReleaseSeatRequest;
-import com.dev.cinemasystem.dto.showTimeSeatDTO.ShowTimeSeatResponse;
-import com.dev.cinemasystem.service.BookingService;
-import jakarta.validation.Valid;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/showtime-seat")
-@RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ShowTimeSeatController {
-
-    BookingService bookingService;
-
-    @GetMapping("/{showTimeId}")
-    public ApiResponse<List<ShowTimeSeatResponse>> getSeatMap(@PathVariable Integer showTimeId) {
-        return ApiResponse.<List<ShowTimeSeatResponse>>builder()
-                .message("Lấy sơ đồ ghế suất chiếu thành công")
-                .result(bookingService.getSeatMap(showTimeId))
-                .build();
-    }
-
-    @PostMapping("/hold")
-    public ApiResponse<HoldSeatResponse> holdSeats(@RequestBody @Valid HoldSeatRequest request) {
-        return ApiResponse.<HoldSeatResponse>builder()
-                .message("Giữ ghế thành công")
-                .result(bookingService.holdSeats(request))
-                .build();
-    }
-
-    @PostMapping("/release")
-    public ApiResponse<HoldSeatResponse> releaseSeats(@RequestBody @Valid ReleaseSeatRequest request) {
-        return ApiResponse.<HoldSeatResponse>builder()
-                .message("Giải phóng ghế thành công")
-                .result(bookingService.releaseSeats(request))
-                .build();
-    }
 }
